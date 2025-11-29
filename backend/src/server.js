199f3c51a64bf19faa6d25cc9b075be722ca9552
Khushi Maheshwari <142376177+Khushiimaheshwari@users.noreply.github.com>
2025-11-29 18:40:57 +0530

    Disable production static file serving
    Comment out production static file serving and catch-all route.

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,13 +32,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-    })
-}
+//     app.get("*", (req,res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+//     })
+// }
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
